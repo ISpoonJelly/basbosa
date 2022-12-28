2b381d469417462f560ced817e5cc2ee018bd161
ISpoonJelly <ispoonjelly@gmail.com>
2022-12-28 17:01:15 +0100

    prevent disconnect when not connected

--- src/commands/disconnect.ts
+++ src/commands/disconnect.ts
@@ -1,10 +1,15 @@
 import { Command, interactionContext } from './command';
 
 export class Disconnect extends Command {
-  public description = 'Salam ya zmeely';
+  public description = 'Etla3 yaba';
 
   public async handleInteraction(ctx: interactionContext) {
     const queue = this.getQueueInSameChannel(ctx);
+
+    if (!queue.connection) {
+      throw Error('Atl3 mnain y3m?');
+    }
+
     queue.destroy(true);
 
     console.log('[Disconnect] Disconnected successfully.');
